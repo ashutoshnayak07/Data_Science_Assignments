966b88742570be2d567cafee08b69e67232e4579
ashutoshnayak07 <ashutoshnayak02@outlook.com>
2026-02-14 22:04:15 +0530

    Commit Message

---
+++ Assignment_Task-1.py
@@ -0,0 +1,11 @@
+#Assignment Task-1
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+Addition=num1+num2
+Subtraction=num1-num2
+Multplication=num1*num2
+Division=num1/num2
+print("Addition :",Addition)
+print("Subtraction :",Subtraction)
+print("Multiplication :",Multplication)
+print("Division :",Division)
---
+++ Assignment_Task-2.py
@@ -0,0 +1,4 @@
+#Assignment Task-2
+F_name=input("Enter your first name: ")
+L_name=input("Enter your last name: ")
+print(f"Hello {F_name} {L_name}! Welcome to Python Program.")
