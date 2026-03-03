fa39149af8a0ad8bc4a5e1ac4d32c3d34357e0b0
ashutoshnayak07 <ashutoshnayak02@outlook.com>
2026-03-03 21:56:03 +0530

    Assignment 2

---
+++ Assignment_2_Task_1.py
@@ -0,0 +1,6 @@
+# Check if a Number is Even or Odd
+num = int(input("Enter a number :"))
+if num % 2 == 0: # If the number is divisible by 2, then it is an Even number.
+    print(f"{num} is an Even number.")
+else: # If the number is not divisible by 2, then it is an Odd number.
+    print(f"{num} is an Odd number.")
